ad7607a56bba87c9cb4a11557658a0b61bd9740b
최익중 <ikjoongchoi@choeigjungs-MacBook-Pro.local>
2022-09-12 20:39:15 +0900

    Learn 4793 완성

--- src/App.js
+++ src/App.js
@@ -1,16 +1,52 @@
 import Button from './Button';
 import HandButton from './HandButton';
+import HandIcon from './HandIcon';
+import { compareHand, generateRandomHand } from './utils';
+import { useState } from 'react';
+
+function getResult(me, other) {
+  const comparison = compareHand(me, other);
+  if (comparison > 0) return '승리';
+  if (comparison < 0) return '패배';
+  return '무승부';
+}
 
 function App() {
-  const handleClick = (value) => console.log(value);
-  const handleClearClick = () => console.log('처음부터');
+  // hand와 otherHand를 state로 바꾸어 주세요
+
+  const [hand, setHand] = useState('rock');
+  const [otherHand, setOtherHand] = useState('rock');
+
+  const handleButtonClick = (nextHand) => {
+    setHand(nextHand);
+    
+    const nextOtherHand = generateRandomHand()
+    setOtherHand(nextOtherHand)
+    
+    // hand의 값을 nextHand 로 바꿔주세요
+    // otherHand의 값을 generateRandomHand()의 리턴 값으로 바꿔주세요
+  };
+
+  const handleClearClick = () => {
+    setHand('rock');
+    setOtherHand('rock');
+    // hand와 otherHand의 값을 'rock' 으로 변경해주세요
+  };
 
   return (
     <div>
       <Button onClick={handleClearClick}>처음부터</Button>
-      <HandButton value="rock" onClick={handleClick} />
-      <HandButton value="scissor" onClick={handleClick} />
-      <HandButton value="paper" onClick={handleClick} />
+      <p>{getResult(hand, otherHand)}</p>
+      <div>
+        <HandIcon value={hand} />
+        VS
+        <HandIcon value={otherHand} />
+      </div>
+      <div>
+        <HandButton value="rock" onClick={handleButtonClick} />
+        <HandButton value="scissor" onClick={handleButtonClick} />
+        <HandButton value="paper" onClick={handleButtonClick} />
+      </div>
     </div>
   );
 }
--- src/HandButton.js
+++ src/HandButton.js
@@ -1,11 +1,12 @@
-import HandIcon from './HandIcon.js';
+import HandIcon from './HandIcon';
 
-const HandButton = ({ value, onClick }) => {
+function HandButton({ value, onClick }) {
   const handleClick = () => onClick(value);
   return (
-    <button onClick={handleClick}
-      ><HandIcon value={value}/>
-    </button>);
+    <button onClick={handleClick}>
+      <HandIcon value={value} />
+    </button>
+  );
 }
 
 export default HandButton;
