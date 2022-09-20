56b961f4729cf1695fe7c02db003faa4dbb4130c
최익중 <ikjoongchoi@choeigjungs-MacBook-Pro.local>
2022-09-21 01:11:27 +0900

    Learn 4738

--- src/App.js
+++ src/App.js
@@ -2,31 +2,36 @@ import {useState} from 'react';
 import Button from './Button';
 import Dice from './Dice';
 
+function random(n) {
+  return Math.ceil(Math.random() * n);
+}
 
 
 
 function App() {
 
-  function random(n) {
-    return Math.ceil(Math.random() * n)
-  };
-
-  const [num, changeNum] = useState(1);
+  const [num, setNum] = useState(1);
+  const [num2, setNum2] = useState(1);
 
   const handleRollClick = () => {
-    const nextNum = random(6)
-    changeNum(nextNum)
-  }  
+    setNum(random(6));
+    setNum2(random(6));
+  };
+
+  const handleClearClick = () => {
+    setNum(1);
+    setNum2(1);
+  };
 
   return (
     <div>
       <div>
         <Button onClick={handleRollClick}>던지기</Button>
-        <Button>처음부터</Button>
+        <Button onClick={handleClearClick}>처음부터</Button>
       </div>
       
       <Dice color="red" num={num} />
-      <Dice color="blue" num={num} />
+      <Dice color="blue" num={num2} />
     </div>
   );
 }
