4486cd452441dd3b283270cd619b4d6b91f0a5f4
최익중 <ikjoongchoi@choeigjungs-MacBook-Pro.local>
2022-10-03 19:04:41 +0900

    Learn 4740 강의

--- src/App.js
+++ src/App.js
@@ -13,14 +13,35 @@ function App() {
   const [num, setNum] = useState(1);
   const [num2, setNum2] = useState(1);
 
+  const [sum, setSum] = useState(0);
+  const [sum2, setSum2] = useState(0);
+
+  const [gameHistory, setGameHistory] = useState([]);
+  const [gameHistory2, setGameHistory2] = useState([]);
+
+
   const handleRollClick = () => {
-    setNum(random(6));
-    setNum2(random(6));
+    const nextNum = random(6);
+    setNum(nextNum);
+    setSum(sum + nextNum);
+    setGameHistory([...gameHistory, nextNum]);
+
+
+
+    const nextNum2 = random(6);
+    setNum2(nextNum2);
+    setSum2(sum2 + nextNum2)
+    setGameHistory2([...gameHistory2, nextNum2]);
   };
 
   const handleClearClick = () => {
     setNum(1);
     setNum2(1);
+    setGameHistory([]);
+  
+    setSum(0);
+    setSum2(0);
+    setGameHistory2([]);
   };
 
   return (
@@ -29,9 +50,18 @@ function App() {
         <Button onClick={handleRollClick}>던지기</Button>
         <Button onClick={handleClearClick}>처음부터</Button>
       </div>
-      
-      <Dice color="red" num={num} />
-      <Dice color="blue" num={num2} />
+      <div>
+        <h2>나</h2>
+        <Dice color="red" num={num} />
+        <h2>총점</h2>
+        <p>{sum}</p>
+        <p>{gameHistory.join(', ')}</p>
+        <h2>너</h2>
+        <Dice color="red" num={num2} />
+        <h2>총점</h2>
+        <p>{sum2}</p>
+        <p>{gameHistory2.join(', ')}</p>
+      </div>
     </div>
   );
 }
