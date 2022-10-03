1accfbad82c89793172f98d94dda9cbacf4da392
최익중 <ikjoongchoi@choeigjungs-MacBook-Pro.local>
2022-10-03 23:05:07 +0900

    Learn 4749 강의

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Board from './Board';
 import {useState} from 'react';
 import Button from './Button';
+import './App.css';
 
 
 function random(n) {
@@ -26,10 +27,10 @@ function App() {
 
 
   return (
-    <div>
+    <div className="App">
       <div>
-        <Button color='blue' onClick={handleRollClick}>던지기</Button>
-        <Button color='red' onClick={handleClearClick}>처음부터</Button>
+        <Button className='App-button' color='blue' onClick={handleRollClick}>던지기</Button>
+        <Button className='App-button' color='red' onClick={handleClearClick}>처음부터</Button>
       </div>
       <div>
         <Board name='나' color='blue' gameHistory={myHistory} />
