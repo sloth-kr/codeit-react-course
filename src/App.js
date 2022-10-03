8db13a82fd2b4f94ebe1f2b336c41b088b09053b
최익중 <ikjoongchoi@choeigjungs-MacBook-Pro.local>
2022-10-03 21:55:00 +0900

    Learn 4765 강의

--- src/App.js
+++ src/App.js
@@ -1,48 +1,29 @@
+import Board from './Board';
 import {useState} from 'react';
 import Button from './Button';
-import Dice from './Dice';
+
 
 function random(n) {
   return Math.ceil(Math.random() * n);
 }
 
-
-
 function App() {
-
-  const [num, setNum] = useState(1);
-  const [num2, setNum2] = useState(1);
-
-  const [sum, setSum] = useState(0);
-  const [sum2, setSum2] = useState(0);
-
-  const [gameHistory, setGameHistory] = useState([]);
-  const [gameHistory2, setGameHistory2] = useState([]);
-
+  const [myHistory, setMyHistory] = useState([]);
+  const [otherHistory, setOtherHistory] = useState([]);
 
   const handleRollClick = () => {
-    const nextNum = random(6);
-    setNum(nextNum);
-    setSum(sum + nextNum);
-    setGameHistory([...gameHistory, nextNum]);
+    const nextMyNum = random(6);
+    setMyHistory([...myHistory, nextMyNum]);
 
-
-
-    const nextNum2 = random(6);
-    setNum2(nextNum2);
-    setSum2(sum2 + nextNum2)
-    setGameHistory2([...gameHistory2, nextNum2]);
+    const nextOtherNum = random(6);
+    setOtherHistory([...otherHistory, nextOtherNum]);
   };
 
   const handleClearClick = () => {
-    setNum(1);
-    setNum2(1);
-    setGameHistory([]);
-  
-    setSum(0);
-    setSum2(0);
-    setGameHistory2([]);
+    setMyHistory([]);
+    setOtherHistory([]);
   };
+
 
   return (
     <div>
@@ -51,16 +32,8 @@ function App() {
         <Button onClick={handleClearClick}>처음부터</Button>
       </div>
       <div>
-        <h2>나</h2>
-        <Dice color="red" num={num} />
-        <h2>총점</h2>
-        <p>{sum}</p>
-        <p>{gameHistory.join(', ')}</p>
-        <h2>너</h2>
-        <Dice color="red" num={num2} />
-        <h2>총점</h2>
-        <p>{sum2}</p>
-        <p>{gameHistory2.join(', ')}</p>
+        <Board name='나' color='blue' gameHistory={myHistory} />
+        <Board name='너' color='red' gameHistory = {otherHistory} />
       </div>
     </div>
   );
