8f877694e281fe9a3c9fb2c1110d1ff7f51956b9
최익중 <ikjoongchoi@choeigjungs-MacBook-Pro.local>
2022-10-03 23:22:06 +0900

    Learn 4750 강의

--- src/HandButton.js
+++ src/HandButton.js
@@ -1,25 +1,13 @@
 import HandIcon from './HandIcon';
+import './HandButton.css'
 import backgroundImg from './assets/purple.svg';
 
-const handButtonStyle = {
-  width: '166px',
-  height: '166px',
-  border: 'none',
-  outline: 'none',
-  textAlign: 'center',
-  cursor: 'pointer',
-  backgroundColor: 'transparent',
-  backgroundImage: `url('${backgroundImg}')`,
-  backgroundRepeat: 'no-repeat',
-  backgroundPosition: 'center',
-  backgroundSize: 'contain',
-}
 
 function HandButton({ value, onClick }) {
   const handleClick = () => onClick(value);
   return (
-    <button style={handButtonStyle} onClick={handleClick}>
-      <HandIcon value={value} />
+    <button className='HandButton' onClick={handleClick}>
+      <HandIcon className='HandButton-icon' value={value} />
     </button>
   );
 }
